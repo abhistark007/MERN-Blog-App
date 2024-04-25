253419d7c98b8b10d98203c5f3e51ba128325fc5
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2024-04-25 19:33:54 +0530

    conncect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 const express=require("express");
 const app=express();
 
+require("dotenv").config();
+const mongoose=require("mongoose");
+mongoose.connect(process.env.DB_URI)
+.then(()=>console.log("DB Connected"))
+.catch((e)=>console.log("DB Failed"));
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
