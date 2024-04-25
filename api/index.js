dfcedc5da961861feb1ac836a1d5cc7417aaf407
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2024-04-25 20:30:23 +0530

    create and test api route

--- api/index.js
+++ api/index.js
@@ -7,7 +7,17 @@ mongoose.connect(process.env.DB_URI)
 .then(()=>console.log("DB Connected"))
 .catch((e)=>console.log("DB Failed"));
 
+const router=require("./routes/user.route");
+app.use("/api/user",router);
+
+
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
+
+
+
+
+
 
