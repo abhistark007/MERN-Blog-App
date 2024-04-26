6cfed782389bf9d1e9f9c5118462e60e00de69d0
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2024-04-26 07:44:48 +0530

    add a middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,15 @@
 const User  = require("../models/user.model");
 const bcryptjs=require("bcryptjs");
+const { errorHandler } = require("../utils/error");
 
 
-exports.signup=async(req,res)=>{
+exports.signup=async(req,res,next)=>{
     // console.log(req.body);
     const {username,email,password}=req.body;
 
     if(!username || !email || !password || username==="" || email==="" || password===""){
-        return res.status(400).json({message:"All fields are required"});
+        // return res.status(400).json({message:"All fields are required"});
+        next(errorHandler(400,'All fields are required'));
     }
     const hashedPassword=await bcryptjs.hash(password,10);
     const newUser=new User({
@@ -21,7 +23,7 @@ exports.signup=async(req,res)=>{
         res.json({message:"Signup successfully"});
     }
     catch(e){
-        res.status(500).json({message:e.message});
+        next(e);
     }
 
 }
--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const mongoose=require("mongoose");
 mongoose.connect(process.env.DB_URI)
 .then(()=>console.log("DB Connected"))
-.catch((e)=>console.log("DB Failed"));
+.catch((e)=>{console.log("DB Failed");console.log(e.message);});
 
 const userRoutes=require("./routes/user.route");
 app.use("/api/user",userRoutes);
@@ -19,6 +19,16 @@ app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
 
+// add a middleware func to handle errors
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || 'Internal server error'
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
 
 
 
